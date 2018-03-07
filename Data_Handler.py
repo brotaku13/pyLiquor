e1b2e5a0bab1221e26030cba803fd51fba58147b
Andrew Ramirez <ars3nx@gmail.com>
2018-03-07 00:04:35 -0800

    Add files via upload

--- Data_Handler.py
+++ Data_Handler.py
@@ -1,12 +1,59 @@
 import pandas as pd
 import numpy as np
+import csv
+#import LiquorView
 
 
 class Data_Handler():
     def __init__(self):
-        self.hi = "Hi"
-        self._table = pd.read_csv('Alcohol_Data/wine.csv')
-    
+        # Wine is the best preview
+        self._wine = pd.read_csv('Alcohol_Data/wine.csv')
+        
+        # Get other files for init
+        self._beer = pd.read_csv('Alcohol_Data/beer_spirits.csv')
+        self._spirits = pd.read_csv('Alcohol_Data/beer_spirits.csv')
+        self._liqour = pd.read_csv('Alcohol_Data/liquor_coolers_ciders_data_sake.csv')
+        self._coolers = pd.read_csv('Alcohol_Data/liquor_coolers_ciders_data_sake.csv')
+        self._sake= pd.read_csv('Alcohol_Data/liquor_coolers_ciders_data_sake.csv')
+        self._cider= pd.read_csv('Alcohol_Data/liquor_coolers_ciders_data_sake.csv')
+
+    # Return all beers in new variable
+    # self._temp['Category'] gives you beers/spirits need a way ti filter out spirits
+    # More efficient to sort if called upon.
+    def get_beer(self):
+        self._beer = (self._beer.loc[self._beer["Category"] == "BEER"])
+        for index, item in self._beer.iterrows():
+            yield item
+
+    def get_spirits(self):
+        self._spirits = (self._spirits.loc[self._spirits["Category"] == "SPIRITS"])
+        for index, item in self._spirits.iterrows():
+            yield item
+            
+    def get_coolers(self):
+        self._coolers = (self._coolers.loc[self._coolers["Category"] == "COOLER"])
+        for index, item in self._coolers.iterrows():
+            yield item
+
+    def get_cider(self):
+        self._cider = (self._cider.loc[self._cider["Category"] == "CIDER"])
+        for index, item in self._cider.iterrows():
+            yield item
+
+    # Can format here instead of other place it was used at.
+    # It will print faster and suggested.
     def get_wine(self):
-        for index, item in self._table.iterrows():
+        for index, item in self._wine.iterrows():
+            yield item
+
+    # Doesn't have correct csv format, no data output
+    # Liqueur is labled Spirit -- Liqour or other -- Liqour
+    def get_liqour(self):
+        self._liqour = (self._liqour.loc[self._liqour["Category"] == "LIQUEUR"])
+        for index, item in self._liqour.iterrows():
+            yield item
+    # Sake is labled Spirit -- Sake or other -- Sake
+    def get_sake(self):
+        self._sake = (self._sake.loc[self._sake["Category"] == "SAKE"])
+        for index, item in self._sake.iterrows():
             yield item
---
+++ Results.py
@@ -0,0 +1,195 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+from Widgets.LiquorDetails import SearchLiquorDetails
+from Widgets.LiquorView import LiquorView
+from Widgets.Search_Widgets.LiquorViewItem import LiquorViewItem
+
+
+class Results(QWidget):
+    
+    back_to_search = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super(Results, self).__init__(parent)
+
+        self._details_view = SearchLiquorDetails()
+        self._liquor_view = LiquorView(["Name"])
+        self._liquor_view.currentItemChanged.connect(self.update_details)
+        
+        self._button_back = QPushButton("Back")
+        self._button_back.clicked.connect(self.on_back_clicked)
+
+        self._details_view.add_to_cabinet.connect(self.add_item_to_cabinet)
+
+        self.define_layout()
+
+    def on_back_clicked(self):
+        #self.clear_tree()
+        self.back_to_search.emit()
+
+    def update_details(self, current_liquor_item: LiquorViewItem):
+        if current_liquor_item is not None:
+            self._details_view.update_ui(current_liquor_item)
+
+    def populate(self, data_handler, search_args):
+        # Clear data before listings.
+        self.clear_tree()
+        # Call on button clicked command.
+        if search_args == 'wine':
+            for entry in data_handler.get_wine():
+                item = LiquorViewItem(entry['ID'],
+                                        entry['Name'].replace('\'', ''),
+                                        entry['Maker'],
+                                        entry['Category'], 
+                                        entry['Sub_Category'], 
+                                        entry['Sub_Sub_Category'],
+                                        entry['Sub_Sub_Sub_Category'],
+                                        float(entry['Cost']),
+                                        float(entry['Volume(ml)']),
+                                        float(entry['Alcohol_By_Volume']),
+                                        entry['Aroma'],
+                                        entry['Color'],
+                                        entry['Origin'],
+                                        entry['Region'])
+                item.setText(0, item.get_name().title())
+                self._liquor_view.addTopLevelItem(item)
+
+        if search_args == 'beer':
+            for entry in data_handler.get_beer():
+                item = LiquorViewItem(entry['ID'],
+                                        entry['Name'].replace('\'', ''),
+                                        entry['Maker'],
+                                        entry['Category'], 
+                                        entry['Sub_Category'], 
+                                        entry['Sub_Sub_Category'],
+                                        entry['Sub_Sub_Sub_Category'],
+                                        float(entry['Cost']),
+                                        float(entry['Volume(ml)']),
+                                        float(entry['Alcohol_By_Volume']),
+                                        entry['Aroma'],
+                                        entry['Color'],
+                                        entry['Origin'],
+                                        entry['Region'])
+                item.setText(0, item.get_name().title())
+                self._liquor_view.addTopLevelItem(item)
+        if search_args == 'spirits':
+            for entry in data_handler.get_spirits():
+                item = LiquorViewItem(entry['ID'],
+                                        entry['Name'].replace('\'', ''),
+                                        entry['Maker'],
+                                        entry['Category'], 
+                                        entry['Sub_Category'], 
+                                        entry['Sub_Sub_Category'],
+                                        entry['Sub_Sub_Sub_Category'],
+                                        float(entry['Cost']),
+                                        float(entry['Volume(ml)']),
+                                        float(entry['Alcohol_By_Volume']),
+                                        entry['Aroma'],
+                                        entry['Color'],
+                                        entry['Origin'],
+                                        entry['Region'])
+                item.setText(0, item.get_name().title())
+                self._liquor_view.addTopLevelItem(item)
+        if search_args == 'liqueur':
+            for entry in data_handler.get_liqour():
+                item = LiquorViewItem(entry['ID'],
+                                        entry['Name'].replace('\'', ''),
+                                        entry['Maker'],
+                                        entry['Category'], 
+                                        entry['Sub_Category'], 
+                                        entry['Sub_Sub_Category'],
+                                        entry['Sub_Sub_Sub_Category'],
+                                        float(entry['Cost']),
+                                        float(entry['Volume(ml)']),
+                                        float(entry['Alcohol_By_Volume']),
+                                        entry['Aroma'],
+                                        entry['Color'],
+                                        entry['Origin'],
+                                        entry['Region'])
+                item.setText(0, item.get_name().title())
+                self._liquor_view.addTopLevelItem(item)
+        if search_args == 'coolers':
+            for entry in data_handler.get_coolers():
+                item = LiquorViewItem(entry['ID'],
+                                        entry['Name'].replace('\'', ''),
+                                        entry['Maker'],
+                                        entry['Category'], 
+                                        entry['Sub_Category'], 
+                                        entry['Sub_Sub_Category'],
+                                        entry['Sub_Sub_Sub_Category'],
+                                        float(entry['Cost']),
+                                        float(entry['Volume(ml)']),
+                                        float(entry['Alcohol_By_Volume']),
+                                        entry['Aroma'],
+                                        entry['Color'],
+                                        entry['Origin'],
+                                        entry['Region'])
+                item.setText(0, item.get_name().title())
+                self._liquor_view.addTopLevelItem(item)
+        if search_args == 'sake':
+            for entry in data_handler.get_sake():
+                item = LiquorViewItem(entry['ID'],
+                                        entry['Name'].replace('\'', ''),
+                                        entry['Maker'],
+                                        entry['Category'], 
+                                        entry['Sub_Category'], 
+                                        entry['Sub_Sub_Category'],
+                                        entry['Sub_Sub_Sub_Category'],
+                                        float(entry['Cost']),
+                                        float(entry['Volume(ml)']),
+                                        float(entry['Alcohol_By_Volume']),
+                                        entry['Aroma'],
+                                        entry['Color'],
+                                        entry['Origin'],
+                                        entry['Region'])
+                item.setText(0, item.get_name().title())
+                self._liquor_view.addTopLevelItem(item)
+        if search_args == 'cider':
+            for entry in data_handler.get_cider():
+                item = LiquorViewItem(entry['ID'],
+                                        entry['Name'].replace('\'', ''),
+                                        entry['Maker'],
+                                        entry['Category'], 
+                                        entry['Sub_Category'], 
+                                        entry['Sub_Sub_Category'],
+                                        entry['Sub_Sub_Sub_Category'],
+                                        float(entry['Cost']),
+                                        float(entry['Volume(ml)']),
+                                        float(entry['Alcohol_By_Volume']),
+                                        entry['Aroma'],
+                                        entry['Color'],
+                                        entry['Origin'],
+                                        entry['Region'])
+                item.setText(0, item.get_name().title())
+                self._liquor_view.addTopLevelItem(item)
+    @pyqtSlot()
+    def add_item_to_cabinet(self):
+        liquor_to_add = self._liquor_view.selectedItems()
+        for item in liquor_to_add:
+            # Prints the result of add to cabinet
+            yield str(item)
+        # : returns QTreeWidgetItem
+
+    def clear_tree(self):
+        for i in reversed(range(self._liquor_view.topLevelItemCount())):
+            self._liquor_view.takeTopLevelItem(i) 
+
+    def define_layout(self):
+        left = QVBoxLayout()
+        button_bar = QHBoxLayout()
+        button_bar.addWidget(self._button_back)
+        button_bar.setAlignment(Qt.AlignLeft)
+
+        left.addWidget(self._liquor_view)
+        left.addLayout(button_bar)
+
+        layout = QHBoxLayout()
+
+        layout.addLayout(left)
+        layout.addWidget(self._details_view)
+        layout.setContentsMargins(0, 0, 0, 11)
+        self.setLayout(layout)
+
+
